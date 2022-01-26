5266ce8ba71a76b92e65cb608e62ff679f2b78a7
hhayden1 <harrisonh1241@gmail.com>
2022-01-25 23:28:01 -0800

    added database

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
 import requests
-import json
 import pandas as pd
 from datetime import datetime
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import sqlite3
 
 HOST = 'https://www.speedrun.com/api/v1/'
 GAME_ID = '268zey6p' # id for majora's mask
+DATABASE_LOCATION = 'sqlite:///player_times.sqlite'
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
 
 def main():
     startTime =  datetime.now()
 
     final_data = add_time()
-    df = pd.DataFrame.from_dict({'Username':final_data.keys(), 'Total Time':final_data.values()})
-    df.sort_values(by=['Total Time'], ascending=False, inplace=True)
-    df['Total Time'] = df['Total Time'].apply(convert_to_hours)
+    df = pd.DataFrame.from_dict({'Username':final_data.keys(), 'TotalTime':final_data.values()})
+    df.sort_values(by=['TotalTime'], ascending=False, inplace=True)
+    df['TotalTime'] = df['TotalTime'].apply(convert_to_hours)
     df.reset_index(drop=True, inplace=True)
 
     df.to_csv('output.csv', index=False)
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('player_times.sqlite')
+    cursor = conn.cursor()
+
+    engine.execute('DROP TABLE IF EXISTS player_times')
+
+    sql_query = '''
+    CREATE TABLE IF NOT EXISTS player_times (
+        ID INTEGER PRIMARY KEY,
+        Username TEXT NOT NULL,
+        TotalTime TEXT NOT NULL
+    )
+    '''
+
+    cursor.execute(sql_query)
+    print('Opened database successfully')
+
+
+    df.to_sql('player_times', engine, index=False, if_exists='append')
+
+
+    conn.close()
+    print('Closed database successfully')
 
     print('Execution time: ' + str(datetime.now() - startTime))
 
